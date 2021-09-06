92379cb71d310d3c2688bdcaa65f81d183366a99
Dwight Ledet <dwightledet@gmail.com>
2021-09-06 07:09:30 +0200

    build first models

--- geoplan/calculator/models.py
+++ geoplan/calculator/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=30)
+    project_location = models.CharField(max_length=30)
+    operator = models.CharField(max_length=30)
+    date_started = models.DateField()
+
+
+class Well(models.Model):
+    well_name = models.CharField(max_length=30)
+    well_location = models.CharField(max_length=30)
